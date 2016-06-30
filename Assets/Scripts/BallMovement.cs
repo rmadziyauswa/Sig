f41e5ebd1b4e5bb1225b5b4dac4442df6bd9a72e
Rennie Madziyauswa <Rennie Madziyauswa>
2016-06-30 14:20:06 +0200

    Initial

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+    public enum BallDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public float magnetStrength = 0.5f;
+    public BallDirection ballDirection = BallDirection.Up;
+    public float ballSpeed = 4.0f;
+    public bool isDead = false;
+    public bool hasReachedDestination = false;
+
+    public Vector2 externalForce = Vector2.zero;
+
+
+    private Rigidbody2D rigidBody2d;
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        rigidBody2d = this.GetComponent<Rigidbody2D>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        HandleInput();
+
+        Movement(externalForce);        
+
+	}
+
+    Vector2 GetVelocity()
+    {
+
+        Vector2  y;
+
+        switch(ballDirection)
+        {
+            case BallDirection.Up:
+                y = Vector2.up * ballSpeed;
+                break;
+
+            case BallDirection.Down:
+                y = Vector2.down * ballSpeed;
+                break;
+            default:
+                y = Vector2.up * ballSpeed;
+                break;
+
+        }
+        
+
+        return y;
+
+
+    }
+
+
+
+    public void Movement(Vector2 externalForce)
+    {
+        Vector2 getVelocity = GetVelocity();
+        
+        Vector2 applyForce = ( externalForce + getVelocity ) * Time.deltaTime;
+
+        rigidBody2d.AddForce(applyForce );
+        
+
+
+    }
+
+  
+
+    public void HandleInput()
+    {
+        var inX = Input.GetAxis("Horizontal") * magnetStrength;
+
+        
+        Vector2 applyForce = new Vector2(inX, 0);
+
+        externalForce = applyForce;
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+
+    private string myTag;
+
+    void Start()
+    {
+        myTag = gameObject.tag;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        string colliderTag = collider.tag;
+
+        if(myTag.Equals("Finish") && colliderTag.Equals("Player"))
+        {
+            //the ball has reached the finish red hole. The Player Has won
+
+            Debug.Log("WINNER!!!");
+
+            return;
+        }
+
+        if(colliderTag.Equals("Player"))
+        {
+            //the player has hit an obstacle
+            Debug.Log("HIT!!!");
+
+        }
+    }
+
+
+     
+
+}
